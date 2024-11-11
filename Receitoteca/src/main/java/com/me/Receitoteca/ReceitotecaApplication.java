c9a0a59a65e8d6c3f04a7eb2861fd5b58f942c67
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-11-11 13:51:06 -0300

    first commit

---
+++ Receitoteca/src/main/java/com/me/Receitoteca/ReceitotecaApplication.java
@@ -0,0 +1,13 @@
+package com.me.Receitoteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitotecaApplication.class, args);
+	}
+
+}
---
+++ Receitoteca/src/test/java/com/me/Receitoteca/ReceitotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.Receitoteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
