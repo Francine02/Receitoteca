41f10070aa1fe25303ff9b31040e0223cf58236f
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-11-11 14:29:25 -0300

    refactor: add preparation and cooking time, category, and difficulty fields

--- Receitoteca/src/main/java/com/me/Receitoteca/entities/Revenue.java
+++ Receitoteca/src/main/java/com/me/Receitoteca/entities/Revenue.java
@@ -2,7 +2,13 @@ package com.me.Receitoteca.entities;
 
 import java.util.List;
 
+import com.me.Receitoteca.enums.Category;
+import com.me.Receitoteca.enums.Difficulty;
+
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,9 +20,23 @@ public class Revenue {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
-    private String category;
+
+    @ElementCollection
     private List<String> ingredients;
+
     private String description;
+
     private String imgUrl;
+
+    private int prepTime;
+
+    private int cookTime;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private Difficulty difficulty;
 }
