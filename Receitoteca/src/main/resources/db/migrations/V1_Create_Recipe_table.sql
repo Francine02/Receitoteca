a5ae67fcc86c2fe80f4d88597b4f2e78416dc65b
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-11-16 17:57:12 -0300

    feat: database connection and create recipe table

---
+++ Receitoteca/src/main/resources/db/migrations/V1_Create_Recipe_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE recipe (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) UNIQUE NOT NULL,
+    ingredients TEXT,
+    description TEXT,
+    img_url VARCHAR(255),
+    prep_time INT,
+    cook_time INT,
+    category VARCHAR(255),
+    difficulty VARCHAR(255)
+);
