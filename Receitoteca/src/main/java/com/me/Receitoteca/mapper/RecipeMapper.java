e4e5287e30d297eeec7c190add0602a6eee6af4b
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-11-16 18:01:17 -0300

    feat: add mapper for conversion between Recipe and RecipeDTO

---
+++ Receitoteca/src/main/java/com/me/Receitoteca/mapper/RecipeMapper.java
@@ -0,0 +1,34 @@
+package com.me.Receitoteca.mapper;
+
+import com.me.Receitoteca.dtos.RecipeDTO;
+import com.me.Receitoteca.entities.Recipe;
+
+public class RecipeMapper {
+    public static RecipeDTO toDTO(Recipe recipe) {
+        return new RecipeDTO(
+                recipe.getId(),
+                recipe.getTitle(),
+                recipe.getIngredients(),
+                recipe.getDescription(),
+                recipe.getImgUrl(),
+                recipe.getPrepTime(),
+                recipe.getCookTime(),
+                recipe.getCategory(),
+                recipe.getDifficulty());
+    }
+
+    public static Recipe toEntity(RecipeDTO recipeDTO) {
+        Recipe recipe = new Recipe();
+        recipe.setId(recipeDTO.id());
+        recipe.setCategory(recipeDTO.category());
+        recipe.setTitle(recipeDTO.title());
+        recipe.setIngredients(recipeDTO.ingredients());
+        recipe.setDescription(recipeDTO.description());
+        recipe.setImgUrl(recipeDTO.imgUrl());
+        recipe.setPrepTime(recipeDTO.prepTime());
+        recipe.setCookTime(recipeDTO.cookTime());
+        recipe.setDifficulty(recipeDTO.difficulty());
+
+        return recipe;
+    }
+}
