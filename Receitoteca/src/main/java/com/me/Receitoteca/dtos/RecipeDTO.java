070ddc9d37482127897d6de46d954e2623661056
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-11-16 18:00:37 -0300

    feat: dto for recipe

---
+++ Receitoteca/src/main/java/com/me/Receitoteca/dtos/RecipeDTO.java
@@ -0,0 +1,18 @@
+package com.me.Receitoteca.dtos;
+
+import java.util.List;
+
+import com.me.Receitoteca.enums.Category;
+import com.me.Receitoteca.enums.Difficulty;
+
+public record RecipeDTO(
+    Long id,
+    String title,
+    List<String> ingredients,
+    String description,
+    String imgUrl,
+    int prepTime,
+    int cookTime,
+    Category category,
+    Difficulty difficulty
+) {}
